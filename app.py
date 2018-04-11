df44ee81daaad1723b0e1f41216df2ecf227b9dc
Kristi Sun <kristisun@kristis-air.home>
2018-04-11 22:53:04 +0100

    Replace API key with your own to this python file

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ print ingredients
 
 print ingredients
 
-url_for_recipes = "https://api.edamam.com/search?app_id=ff6d4efa&app_key=f70aa029996a664dcb7516a504f11d74&q={}".format(" ".join(ingredients))
+url_for_recipes = "https://api.edamam.com/search?app_id=INSERTHERE&app_key=INSERTHERE&q={}".format(" ".join(ingredients))
 
 response = requests.get(url_for_recipes)
 
