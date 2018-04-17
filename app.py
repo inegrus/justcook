3bed6225252ac9ffcaea6a886c5b2da3d0674014
ioana <ravarioana@gmail.com>
2018-04-17 21:33:21 +0100

    Set host to 0.0.0.0

--- app.py
+++ app.py
@@ -56,5 +56,6 @@ def search():
 
     return render_template("recipe.html", response = response )
 
-port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', debug=True, port=port)
