27c63009fc8eaf9671d301654994cc06841dbeb4
ioana <ravarioana@gmail.com>
2018-04-18 12:50:39 +0100

    Host added in the right place

--- app.py
+++ app.py
@@ -79,5 +79,6 @@ def suggest():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
-    host='0.0.0.0'
+    # app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
+    
